1614957ad7b183f9058da473e5d0d1bbe2e37648
yusuferens <yusuferenketen@gmail.com>
2024-01-15 21:42:38 +0300

    finished the project

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const openModalBtn = document.getElementById('openModalBtn');
 const addBookModal = document.getElementById('addBookModal');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const addBookForm = document.getElementById('addBookForm');
-const sumbitBookBtn = document.getElementById('submitBookBtn')
+const submitBookBtn = document.getElementById('submitBookBtn');
+const cardsContainer = document.querySelector('.cardscontainer');
 
 openModalBtn.addEventListener('click', () => {
     addBookModal.showModal();
@@ -13,19 +14,42 @@ closeModalBtn.addEventListener('click', () => {
     addBookModal.close();
 });
 
+submitBookBtn.addEventListener('click', () => {
+    const title = document.getElementsByName('title')[0].value;
+    const author = document.getElementsByName('author')[0].value;
+    const pages = document.getElementsByName('pages')[0].value;
 
-function Book(title, author, numberOfPages) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-}
+    if (title && author && pages) {
+        addBookToLibrary(title, author, parseInt(pages));
+        displayBooks();
+        addBookModal.close();
+    } else {
+        alert("Please fill in all fields.");
+    }
+});
 
 function addBookToLibrary(title, author, numberOfPages) {
-    const newBook = new Book(title, author, numberOfPages);
+    const newBook = { title, author, numberOfPages };
     myLibrary.push(newBook);
-
 }
 
-addBookToLibrary("The book", "Jeff", 199);
+function displayBooks() {
+    cardsContainer.innerHTML = '';
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `<h2>Title: ${book.title}</h2>
+                          <h2>Author: ${book.author}</h2>
+                          <h2>Pages: ${book.numberOfPages}</h2>
+                          <button class="deleteBtn" onclick="deleteBook('${book.title}')"></button>`;
+        cardsContainer.appendChild(card);
+    });
+}
 
-console.log(myLibrary);
+function deleteBook(title) {
+    const index = myLibrary.findIndex(book => book.title === title);
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
+displayBooks();
